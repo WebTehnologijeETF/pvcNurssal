bbee18f5937248a9d5ea0b455a721362cc45b108
adautbegovic <adautbegov1@etf.unsa.ba>
2015-05-14 22:34:10 +0200

    Update SlanjeMaila.php

--- SlanjeMaila.php
+++ SlanjeMaila.php
@@ -2,10 +2,8 @@
 
 require("sendgrid-php/sendgrid-php.php");
 $eol = PHP_EOL;
-//PROMJENITI PODATKE!!!!!!!!!!!!!!!!!!!
 $service_plan_id = "sendgrid_a4d39";
 $account_info = json_decode(getenv($service_plan_id), true);
-//PROMJENITI PODATKE!!!!!!!!!!!!!!!!!!!!!
 $sendgrid = new SendGrid("amrad", "1DvaTri!");
 $email    = new SendGrid\Email();
 $message = "Ime i prezime: ".$_POST['imeVrijednost']." " .$_POST['prezimeVrijednost'] . "\r\n"."Email: ".$_POST['mailVrijednost']."\r\n"."Grad: ".$_POST['gradVrijednost']." ".$_POST['postanskiVrijednost']."\r\n"."\r\n"."\r\n".$_POST['porukaVrijednost'];
